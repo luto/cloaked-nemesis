615707db6ce41ded2b8c7553bcdcd18679e6b9b3
mluto <m@luto.at>
2013-02-03 10:47:38 +0100

    Download deps via script

---
+++ getDeps.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+npm install socket.io
+npm install node-static
+
+cd static/js/
+
+wget "http://jindo.dev.naver.com/collie/download.php?name=collie.min" -O collie.min.js
+wget "http://code.jquery.com/jquery-1.9.0.min.js" -O jquery-1.9.0.min.js
