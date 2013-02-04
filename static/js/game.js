f0f5833aa443617407f311e0f193f4bb6b5940fa
mluto <m@luto.at>
2013-02-04 20:58:44 +0100

    Code reformatting

--- static/js/game.js
+++ static/js/game.js
@@ -9,47 +9,47 @@ var c_players = {};
 
 exports.init = function()
 {
-	comm.init();
-	comm.listen('PLAYER_JOINED', handlePlayerJoined);
-	comm.listen('PLAYER_LEFT', handlePlayerLeft);
-	comm.listen('PHYSICS', handlePhysicsUpdate);
-	comm.listen('HELLO', handleHello);
+  comm.init();
+  comm.listen('PLAYER_JOINED', handlePlayerJoined);
+  comm.listen('PLAYER_LEFT', handlePlayerLeft);
+  comm.listen('PHYSICS', handlePhysicsUpdate);
+  comm.listen('HELLO', handleHello);
 };
 
 function handleHello(data)
 {
-	document.addEventListener('keydown', handleKeyDown);
-	
-	c_layer_players = new collie.Layer({ width: data.worldSize.width,
-		                                   height: data.worldSize.height });
-	
-	collie.Renderer.addLayer(c_layer_players);
-	collie.Renderer.load($("#game")[0]);
-	collie.Renderer.start("30fps");
-};
+  document.addEventListener('keydown', handleKeyDown);
+  
+  c_layer_players = new collie.Layer({ width: data.worldSize.width,
+                                       height: data.worldSize.height });
+  
+  collie.Renderer.addLayer(c_layer_players);
+  collie.Renderer.load($("#game")[0]);
+  collie.Renderer.start("30fps");
+}
 
 function handlePlayerJoined(entity)
 {
-	entity = types.getObj(entity.__type, entity);
-	entities[entity.id] = entity;
-	
-	if(entity instanceof types.t_Player)
-	  createPlayer(entity);
+  entity = types.getObj(entity.__type, entity);
+  entities[entity.id] = entity;
+  
+  if(entity instanceof types.t_Player)
+    createPlayer(entity);
 }
 
 function createPlayer(player)
 {
-	var c_player = new collie.MovableObject(
-		{
-			width : player.width,
-			height : player.height,
-			mass: 1,
-			backgroundColor: player.color,
-		});
-	
-	c_players[player.id] = c_player;
-	setPlayerPos(player);
-	c_player.addTo(c_layer_players);
+  var c_player = new collie.MovableObject(
+    {
+      width : player.width,
+      height : player.height,
+      mass: 1,
+      backgroundColor: player.color,
+    });
+  
+  c_players[player.id] = c_player;
+  setPlayerPos(player);
+  c_player.addTo(c_layer_players);
 }
 
 function handlePlayerLeft(data)
@@ -61,41 +61,41 @@ function handlePlayerLeft(data)
 
 function handlePhysicsUpdate(data)
 {
-	for(var id in data.bodies)
-	{
-	  entities[id].x = data.bodies[id].x;
-	  entities[id].y = data.bodies[id].y;
-	  
-	  setPlayerPos(entities[id]);
-	}
+  for(var id in data.bodies)
+  {
+    entities[id].x = data.bodies[id].x;
+    entities[id].y = data.bodies[id].y;
+    
+    setPlayerPos(entities[id]);
+  }
 }
 
 function setPlayerPos(player)
 {
-	c_players[player.id].set(
-		{
-			x : player.x,
-			y : player.y,
-		});
+  c_players[player.id].set(
+    {
+      x : player.x,
+      y : player.y,
+    });
 }
 
 function handleKeyDown(evt)
 {
-	var dist = 300;
-	if(evt.keyCode == 37) // left
-	{
-		comm.sendMove(dist * -1, 0);
-	}
-	else if(evt.keyCode == 38) // up
-	{
-		comm.sendMove(0, dist * -1);
-	}
-	else if(evt.keyCode == 39) // right
-	{
-		comm.sendMove(dist, 0);
-	}
-	else if(evt.keyCode == 40) // down
-	{
-		comm.sendMove(0, dist);
-	}
+  var dist = 300;
+  if(evt.keyCode == 37) // left
+  {
+    comm.sendMove(dist * -1, 0);
+  }
+  else if(evt.keyCode == 38) // up
+  {
+    comm.sendMove(0, dist * -1);
+  }
+  else if(evt.keyCode == 39) // right
+  {
+    comm.sendMove(dist, 0);
+  }
+  else if(evt.keyCode == 40) // down
+  {
+    comm.sendMove(0, dist);
+  }
 }
