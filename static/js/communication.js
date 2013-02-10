46f34a380e21831771a113e21df56450784eaf3f
mluto <m@luto.at>
2013-02-10 16:11:43 +0100

    Removed tabs...

--- static/js/communication.js
+++ static/js/communication.js
@@ -4,27 +4,27 @@ var eventListeners = [];
 exports.init = function ()
 {
   eventListeners["HELLO"] = [];
-	eventListeners["PLAYER_JOINED"] = [];
-	eventListeners["PLAYER_LEFT"] = [];
-	eventListeners["PHYSICS"] = [];
-	
-	socket = io.connect('http://' + window.location.hostname + ':' + window.location.port);
-	socket.emit('HELLO', { name: 'luto' });
-	socket.on('HELLO', onHello);
-	socket.on('PAK', onPacket);
+  eventListeners["PLAYER_JOINED"] = [];
+  eventListeners["PLAYER_LEFT"] = [];
+  eventListeners["PHYSICS"] = [];
+  
+  socket = io.connect('http://' + window.location.hostname + ':' + window.location.port);
+  socket.emit('HELLO', { name: 'luto' });
+  socket.on('HELLO', onHello);
+  socket.on('PAK', onPacket);
 };
 
 exports.listen = function (evt, func)
 {
-	if(!eventListeners[evt])
- 	 throw "No such event!";
+  if(!eventListeners[evt])
+    throw "No such event!";
 
- 	eventListeners[evt].push(func);
+   eventListeners[evt].push(func);
 }
 
 exports.sendMove = function (x, y)
 {
-	socket.emit('PAK', { type: 'MOVE', data: { x: x, y: y }});
+  socket.emit('PAK', { type: 'MOVE', data: { x: x, y: y }});
 }
 
 function onHello(pak)
@@ -34,13 +34,13 @@ function onHello(pak)
 
 function onPacket(pak)
 {
-	triggerEvent(pak.type, pak.data);
+  triggerEvent(pak.type, pak.data);
 }
 
 function triggerEvent(evt, arg)
 {
-	for(var func in eventListeners[evt])
-	{
-		eventListeners[evt][func](arg);
-	}
+  for(var func in eventListeners[evt])
+  {
+    eventListeners[evt][func](arg);
+  }
 }
