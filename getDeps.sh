86631e8a49d5f77ebbd36d82abf56b616aa8b3ce
mluto <m@luto.at>
2013-02-15 12:18:25 +0100

    Create directories in getDeps.sh

--- getDeps.sh
+++ getDeps.sh
@@ -15,19 +15,23 @@ fi
 
 sed "s/var Box2D = {};/Box2D = {};/" Box2dWeb-2.1.a.3.js
 
-cd static/js/
+cd static
 
+mkdir js
+cd js
 wget "http://jindo.dev.naver.com/collie/download.php?name=collie.min" -O collie.min.js
 wget "http://code.jquery.com/jquery-1.9.0.min.js" -O jquery-1.9.0.min.js
 wget "https://raw.github.com/mattsnider/jquery-plugin-query-parser/master/jquery-queryParser.min.js" -O jquery-queryParser.min.js
 wget "https://raw.github.com/claviska/jquery-miniColors/master/jquery.minicolors.js" -O jquery.minicolors.js
-cd ../..
+cd ..
 
-cd static/img
+mkdir img
+cd img
 wget "https://raw.github.com/claviska/jquery-miniColors/master/jquery.minicolors.png" -O jquery.minicolors.png
-cd ../..
+cd ..
 
-cd static/css
+mkdir css
+cd css
 wget "https://raw.github.com/claviska/jquery-miniColors/master/jquery.minicolors.css" -O jquery.minicolors.css
 sed "s/jquery.minicolors.png/\/static\/img\/jquery.minicolors.png/" jquery.minicolors.css
-cd ../..
+cd ..
