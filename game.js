cad3339b946b19dd30c696d2c8afb45ca9dabb8c
mluto <m@luto.at>
2013-02-16 20:21:57 +0100

    Check the format of the supplied uid

--- game.js
+++ game.js
@@ -60,6 +60,12 @@ exports.onNewPlayer = function (data, cb)
     return;
   }
 
+  if(!data.uid.match(/^[a-f0-9]{8}\-[a-f0-9]{4}\-4[a-f0-9]{3}\-[a-f0-9]{4}\-[a-f0-9]{12}$/))
+  {
+    cb(null, "uid-invalid");
+    return;
+  }
+
   for(var id in entities)
   {
     if(entities[id] instanceof types.t_Player)
